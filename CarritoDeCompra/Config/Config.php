c2e071283d565183caf5fea07a2305dc33577966
Marcos <166460088+M4rk0Zoof@users.noreply.github.com>
2024-08-23 18:59:45 -0300

    Add files via upload

---
+++ CarritoDeCompra/Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/CarritoDeCompra/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "Carrito";
+const CHARSET = "charset=utf8";
+const TITLE = "Carrito de compra";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+?>
