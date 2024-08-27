cec0c0b215688a4672abaf27037212fc665a753c
crisalixis <164426470+crisalixis@users.noreply.github.com>
2024-08-26 21:26:43 -0300

    Add files via upload

--- details.php
+++ details.php
@@ -71,7 +71,7 @@ if($id == '' || $token == ''){
 <header data-bs-theme="dark">
   <div class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a href="#" class="navbar-brand">
+      <a href="index.php" class="navbar-brand">
         <strong>Tienda deportiva</strong>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
